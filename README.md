0a22519f00ab6dbce32e556e96ea2c07dbf1ad2e
papaya213 <papaya0213@foxmail.com>
2024-08-06 15:23:28 +0800

    update the translator to use chatglm2-6b model

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # AI 大模型应用开发实战营 -- 第八周作业
 
 ## 实现功能：
-  
+    基于 ChatGLM2-6B 实现带图形化界面的 openai-translator。该实现基于课程内容langchain的openai-translator修改，将后台模型改为本地部署的[ChatGLM2-6B](https://github.com/THUDM/ChatGLM2-6B)。
 ## 运行方法：
+    1. 运行ChatGLM2-6B提供的api。如hugging face直接拉取模型有困难，需要将模型改为本地路径。当前默认访问本地默认服务端口8000:
+        ChatGLM2-6B> python api.py
+    2. 运行openai-translator的web UI。
+        openai-translator> python ai_translator\gradio_server.py
+    3. 将文件拖拽或者选择上传到系统，在翻译完成后下载翻译文件。
--- openai-translator/ai_translator/translator/translation_chain.py
+++ openai-translator/ai_translator/translator/translation_chain.py
@@ -1,30 +1,33 @@
-from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain_community.llms import ChatGLM
+from langchain_core.prompts import PromptTemplate
+
+# import os
 
 from utils import LOG
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
 
 class TranslationChain:
-    def __init__(self, model_name: str = "gpt-3.5-turbo", verbose: bool = True):
+    def __init__(self, model_name: str = "chatglm2-6b", verbose: bool = True):
         
-        # 翻译任务指令始终由 System 角色承担
-        template = (
-            """You are a translation expert, proficient in various languages. \n
-            Translates {source_language} to {target_language}."""
+        template = """你是一个翻译专家，请将下文从{source_language}翻译为{target_language}. 需要翻译的文本如下： {text} """
+
+
+        chat_prompt_template = ChatPromptTemplate.from_template(template)
+
+        # 本地部署ChatGLM2-6B
+        endpoint_url = "http://localhost:8000"
+
+        # direct access endpoint in a proxied environment
+        # os.environ['NO_PROXY'] = '127.0.0.1'
+
+        chat = ChatGLM(
+            endpoint_url=endpoint_url,
+            max_token=80000,
+            top_p=0.9,
+            temperature=0,
+            model_kwargs={"sample_model_args": False},
         )
-        system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-
-        # 待翻译文本由 Human 角色输入
-        human_template = "{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-        # 使用 System 和 Human 角色的提示模板构造 ChatPromptTemplate
-        chat_prompt_template = ChatPromptTemplate.from_messages(
-            [system_message_prompt, human_message_prompt]
-        )
-
-        # 为了翻译结果的稳定性，将 temperature 设置为 0
-        chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
 
         self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
